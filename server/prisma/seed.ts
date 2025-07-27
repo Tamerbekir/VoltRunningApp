98b556a8d5dc01d76609dc3b085e441c3c573804
Tamerbekir <astoryofame@gmail.com>
2025-07-26 21:47:08 -0400

    Added seed, generated prisma, added db and changed files

---
+++ server/prisma/seed.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcryptjs'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const hashedPassword = await bcrypt.hash('password', 10)
+
+  await prisma.user.create({
+    data: {
+      email: 'test@test.com',
+      password: hashedPassword
+    }
+  })
+  console.log('User seed')
+}
+
+main().catch((error => console.log(error))).finally(() => prisma.$disconnect())
